609e6192b67444f02c383d5092b8ca5cf15262c6
lana <artist158@naver.com>
2022-10-18 12:19:53 +0900

    2022.10.18 intent memo send

--- app/src/main/java/com/example/umc_study/MainActivity.kt
+++ app/src/main/java/com/example/umc_study/MainActivity.kt
@@ -8,6 +8,8 @@ import com.example.umc_study.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
 
+    var memoContents:String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,8 +17,18 @@ class MainActivity : AppCompatActivity() {
 
         viewBinding.sendButton.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra("main_text", viewBinding.memoContent.text)
+            intent.putExtra("main_text", viewBinding.memoContent.text.toString())
             startActivity(intent)
         }
     }
+
+    override fun onPause() {
+        super.onPause()
+        memoContents = viewBinding.memoContent.text.toString()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
 }
