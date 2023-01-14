03bfbcd63b8a434e5d1de47ddee91442c6075caf
lana <artist158@naver.com>
2023-01-14 18:01:57 +0900

    project default

---
+++ new/app/src/main/java/com/example/newmemo/CreateMemo.kt
@@ -0,0 +1,11 @@
+package com.example.newmemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateMemo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_memo)
+    }
+}
