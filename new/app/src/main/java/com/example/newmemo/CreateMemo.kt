a8f3da887768b36de18db5d75a17c069e070438a
lana <artist158@naver.com>
2023-01-14 19:16:46 +0900

    ..? MVVM < MVP ..?? week4...

--- new/app/src/main/java/com/example/newmemo/CreateMemo.kt
+++ new/app/src/main/java/com/example/newmemo/CreateMemo.kt
@@ -2,10 +2,17 @@ package com.example.newmemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.example.newmemo.databinding.ActivityCreateMemoBinding
 
 class CreateMemo : AppCompatActivity() {
+    private val viewModel: NewMemo by viewModels()
+    private lateinit var binding: ActivityCreateMemoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_memo)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_memo)
+        binding.lifecycleOwner = this
     }
 }
--- new/app/src/main/java/com/example/newmemo/NewMemo.kt
+++ new/app/src/main/java/com/example/newmemo/NewMemo.kt
@@ -3,5 +3,10 @@ package com.example.newmemo
 import androidx.lifecycle.ViewModel
 
 class NewMemo : ViewModel() {
+    private var memoTitle:String = ""
+    private var memoContent:String = ""
 
+    fun saveMemo() {
+
+    }
 }
